191c5b0c0aad97d1da085413d572b18ef20d783a
Tsundere Chen <me@tsunderechen.io>
2020-06-28 02:44:34 +0800

    Add header and A record for TypeAHandler

--- handler/TypeA.go
+++ handler/TypeA.go
@@ -1,11 +1,16 @@
 package handler
 
 import (
-    //"net"
+    "net"
     "github.com/miekg/dns"
 )
 
 func TypeAHandler(domain string)(rr dns.A){
-    // WIP
+    rr.Hdr = dns.RR_Header{
+        Name: domain,
+        Rrtype: dns.TypeA,
+        Class: dns.ClassINET,
+        Ttl: 3600}
+    rr.A = net.IPv4(127,0,0,1)
     return rr
 }
