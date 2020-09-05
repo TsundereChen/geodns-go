2f54715fbec7b356f90646946ffba1c41d647b8d
Tsundere Chen <me@tsunderechen.io>
2020-09-06 02:09:53 +0800

    Add TypeA RR generator

---
+++ pkg/rr/A.go
@@ -0,0 +1,17 @@
+package rr
+
+import (
+    "github.com/miekg/dns"
+    "net"
+)
+
+func TypeA(fqdn string, value string) (Rr *dns.A){
+    Rr = new(dns.A)
+    Rr.Hdr = dns.RR_Header{
+        Name: fqdn,
+        Rrtype: dns.TypeA,
+        Class: dns.ClassINET,
+        Ttl: 3600}
+    Rr.A = net.ParseIP(value)
+    return Rr
+}
