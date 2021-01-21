fd424a8cc1ecb6b9668aa8339934670972889f2c
Tsundere Chen <me@tsunderechen.io>
2021-01-21 16:07:08 +0800

    fix(pkg/handler/generator.go): Add debug message in function RrGenerator

--- pkg/handler/generator.go
+++ pkg/handler/generator.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+	"github.com/TsundereChen/geodns-go/pkg/config"
 	rrLib "github.com/TsundereChen/geodns-go/pkg/rr"
 	"github.com/miekg/dns"
 )
@@ -8,15 +10,27 @@ import (
 func RrGenerator(requestType uint16, fqdn string, value string) (rr dns.RR) {
 	switch requestType {
 	case dns.TypeA:
+		if *(config.Debug) == true {
+			fmt.Printf("Generate A record for %s\n", value)
+		}
 		rr = rrLib.TypeA(fqdn, value)
 		break
 	case dns.TypeAAAA:
+		if *(config.Debug) == true {
+			fmt.Printf("Generate AAAA record for %s\n", value)
+		}
 		rr = rrLib.TypeAAAA(fqdn, value)
 		break
 	case dns.TypeCNAME:
+		if *(config.Debug) == true {
+			fmt.Printf("Generate CNAME record for %s\n", value)
+		}
 		rr = rrLib.TypeCNAME(fqdn, value)
 		break
 	case dns.TypeTXT:
+		if *(config.Debug) == true {
+			fmt.Printf("Generate TXT record for %s\n", value)
+		}
 		rr = rrLib.TypeTXT(fqdn, value)
 		break
 	default:
