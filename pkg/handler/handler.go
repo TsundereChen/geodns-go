e6dbbcc4873b9301177d268882383a9d61fd0480
Tsundere Chen <me@tsunderechen.io>
2021-02-24 15:40:09 +0800

    fix(pkg/handler/handler.go): Use log instead of fmt

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/TsundereChen/geodns-go/pkg/config"
 	"github.com/TsundereChen/geodns-go/pkg/fetch"
 	"github.com/miekg/dns"
@@ -17,9 +16,9 @@ func DNSHandler(fqdn string, questionType uint16, sourceAddress net.IP, IPv4 boo
 		log.Panic(err)
 	}
 	if *(config.Debug) == true {
-		fmt.Printf("Source IP => %s\n", sourceAddress.String())
-		fmt.Printf("Source Country => %s\n", record.Country.IsoCode)
-		fmt.Printf("handler.DNSHandler handling request %s, question type %s\n", fqdn, dns.TypeToString[questionType])
+		log.Printf("Source IP => %s\n", sourceAddress.String())
+		log.Printf("Source Country => %s\n", record.Country.IsoCode)
+		log.Printf("handler.DNSHandler handling request %s, question type %s\n", fqdn, dns.TypeToString[questionType])
 	}
 	var value string
 	value = ""
@@ -28,7 +27,7 @@ func DNSHandler(fqdn string, questionType uint16, sourceAddress net.IP, IPv4 boo
 			// Split FQDN into domain and subdomain
 			subdomain := fetch.FetchSubDomainName(fqdn, k)
 			if *(config.Debug) == true {
-				fmt.Printf("Searching subdomain %s in %s\n", subdomain, k)
+				log.Printf("Searching subdomain %s in %s\n", subdomain, k)
 			}
 			// Now find if record exists.
 			rrData := fetch.FetchRR(config.ConfigMap[k])
