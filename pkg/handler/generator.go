471d69c9e4e53f63091e27ca2222fe80419a023c
Tsundere Chen <me@tsunderechen.io>
2021-03-27 14:09:42 +0800

    style(pkg/handler/generator.go): Fix grammar

--- pkg/handler/generator.go
+++ pkg/handler/generator.go
@@ -11,25 +11,25 @@ func RrGenerator(requestType uint16, fqdn string, value string) (rr dns.RR) {
 	switch requestType {
 	case dns.TypeA:
 		if *(config.Debug) == true {
-			log.Printf("Generate A record for %s\n", value)
+			log.Printf("Generating A record for %s\n", value)
 		}
 		rr = rrLib.TypeA(fqdn, value)
 		break
 	case dns.TypeAAAA:
 		if *(config.Debug) == true {
-			log.Printf("Generate AAAA record for %s\n", value)
+			log.Printf("Generating AAAA record for %s\n", value)
 		}
 		rr = rrLib.TypeAAAA(fqdn, value)
 		break
 	case dns.TypeCNAME:
 		if *(config.Debug) == true {
-			log.Printf("Generate CNAME record for %s\n", value)
+			log.Printf("Generating CNAME record for %s\n", value)
 		}
 		rr = rrLib.TypeCNAME(fqdn, value)
 		break
 	case dns.TypeTXT:
 		if *(config.Debug) == true {
-			log.Printf("Generate TXT record for %s\n", value)
+			log.Printf("Generating TXT record for %s\n", value)
 		}
 		rr = rrLib.TypeTXT(fqdn, value)
 		break
